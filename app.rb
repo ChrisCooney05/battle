a3cc94dbdd88ed9414bb18435b53c34a027e2701
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-28 15:07:40 +0100

    play.erb added + test for name input

--- app.rb
+++ app.rb
@@ -3,8 +3,18 @@ require "sinatra/base"
 class Battle < Sinatra::Base
 
 get '/' do
-  'Hello Battle!'
+  erb :index
+end
+
+post '/names' do
+  @player1 = params[:player1]
+  @player2 = params[:player2]
+  erb :battle
 end
 
   run! if app_file == $0
 end
+
+# As two Players,
+# So we can play a personalised game of Battle,
+# We want to Start a fight by entering our names and seeing them
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,4 +4,12 @@ feature 'Infrastructure test' do
   visit('/')
   expect(page).to have_content('Hello Battle!')
  end
+
+ scenario "Displays both player names, entered into forms" do
+   visit('/')
+   fill_in('player1', with: 'Dave')
+   fill_in('player2', with: 'John')
+   click_button('Submit')
+   expect(page).to have_content('Dave vs John')
+ end
 end
